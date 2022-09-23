2bdec6d52eb1566825fa56c3cec1e41951cb7fba
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-23 16:29:13 +0200

    add read and write stream

---
+++ Address.cs
@@ -0,0 +1,31 @@
+﻿public class Address
+{
+    public string name;
+    public string surname;
+    public string street;
+    public string city;
+    public string province;
+    public int zip;
+    public Address(string name, string surname, string street, string city, string province, int zip)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.street = street;
+        this.city = city;
+        this.province = province;
+        this.zip = zip;
+    }
+    public override string ToString()
+    {
+        string line = "//-----------------Indirizzo-----------------//\n";
+        line += "Nome: " + name + "\n";
+        line += "Cognome: " + surname + "\n";
+        line += "Indirizzo: " + street + "\n";
+        line += "Città: " + city + "\n";
+        line += "Provincia: " + province + "\n";
+        line += "CAP: " + zip + "\n";
+        line += "//-----------------*********-----------------//\n";
+
+        return line;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+﻿//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
 //In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 //Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
 
+StreamReader fileAddress = File.OpenText("C:\\Users\\Francesco\\Desktop\\Dot Net Experis\\Tutorati\\Esercizio-7\\csharp-lista-indirizzi\\addresses.csv");
+List<Address> listAddress = new List<Address>();
+fileAddress.ReadLine();
+
+while (!fileAddress.EndOfStream)
+{
+    string riga = fileAddress.ReadLine();
+
+    //char[] 
+
+    string[] split = riga.Split(',');    
+
+    try
+    {
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        int zip = int.Parse(split[5]);
+
+        Address address = new Address(name, surname, street, city, province, zip);
+        listAddress.Add(address);
+        Console.WriteLine(address);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Indirizzo non valido -->" + riga + "\n");
+    }
+}
+
+fileAddress.Close();
